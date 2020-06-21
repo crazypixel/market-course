c4c0d6b3c821b1b7d2951872a05bb9edfcf2cedf
Shay Keinan <crazypixel@gmail.com>
2020-06-21 12:06:16 +0300

    first react components and iteration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const items = ['Milk', 'Eggs', 'Bread'];
+const App = () => (
+	<div>
+		<h1>My List</h1>
+		<List>
+			{items.map(item => <Item key={item} label={item}/>)}
+		</List>
+	</div>
 );
+
+const List = ({children}) => <ul>{children}</ul>;
+
+const Item = ({label}) => <li style={{color: 'red'}}>{label}</li>;
+
+ReactDOM.render(
+	<App/>,
+	document.getElementById('root')
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
